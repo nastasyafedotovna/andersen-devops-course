ee2f622f0837fb48f61deacccbd924a1babb2446
nastsya fedotovna <gizar.zigangirov@gmail.com>
2021-06-24 17:00:35 +0300

    Update docker task

--- docker_task/task.sh
+++ docker_task/task.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 name="docker_task_gizar"
-docker build -t ${name} .
-docker run --rm -dp 8080:8080 --name ${name} ${name}
-if [[ ! $? -ne 0 ]];then
+docker image build -t ${name} .
+docker container run --rm -dp 8080:8080 --name ${name} ${name}
+status=$(docker inspect ${name} --format '{{ .State.Status }}')
+if [[ $status=="running" ]];then
   sleep 1
   curl -XPOST -d'{"animal":"dog", "sound":"bark", "count": 6}' 127.0.0.1:8080
-  docker stop ${name}
+  docker container stop ${name}
 fi
