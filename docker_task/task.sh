e34b45427c50064c885a7c07442551095afb589c
nastsya fedotovna <gizar.zigangirov@gmail.com>
2021-06-24 17:01:19 +0300

    fix script

--- docker_task/task.sh
+++ docker_task/task.sh
@@ -4,7 +4,6 @@ docker image build -t ${name} .
 docker container run --rm -dp 8080:8080 --name ${name} ${name}
 status=$(docker inspect ${name} --format '{{ .State.Status }}')
 if [[ $status=="running" ]];then
-  sleep 1
   curl -XPOST -d'{"animal":"dog", "sound":"bark", "count": 6}' 127.0.0.1:8080
   docker container stop ${name}
 fi
